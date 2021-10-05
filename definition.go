471f016f50ab4ae6499f2e9613b7d1308abb3f22
zikwall <andrey.kapitonov.96@gmail.com>
2021-10-05 19:22:48 +0300

    review

--- definition.go
+++ definition.go
@@ -4,17 +4,6 @@ import "context"
 
 // Worker A worker interface that provides a synchronous Perform method
 // for the ability to implement custom processing of an asynchronous task.
-//
-// func (w *MockWorker) Perform(ctx context.Context, id int) {
-//	for { <- Observable
-//		select {
-//		case <-ctx.Done(): <- Providable
-//			return
-//		}
-//
-//		do stuff...
-//	}
-// }
 type Worker interface {
 	Perform(context.Context, WorkerStream)
 	Name() string
